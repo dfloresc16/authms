fb1012c4e5c1f450888f1aad156207cd86c6328c
Diego Flores <dfloresc1602@alumno.ipn.mx>
2024-12-13 11:27:13 -0600

    fix: quitando archivo de logs

--- src/main/java/com/pt/authms/AuthmsApplication.java
+++ src/main/java/com/pt/authms/AuthmsApplication.java
@@ -2,29 +2,12 @@ package com.pt.authms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.slf4j.LoggerFactory;
-import ch.qos.logback.classic.LoggerContext;
-import ch.qos.logback.classic.util.ContextInitializer;
 
 @SpringBootApplication
 public class AuthmsApplication {
 
-    public static void main(String[] args) {
-        // Configura manualmente el contexto de Logback para establecer LOG_PATH
-        LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
-        context.reset(); // Resetea cualquier configuración previa
+	public static void main(String[] args) {
+		SpringApplication.run(AuthmsApplication.class, args);
+	}
 
-        // Define el directorio donde se generarán los logs
-        System.setProperty("LOG_PATH", "./logs");
-
-        // Configura Logback automáticamente con el contexto inicializado
-        try {
-            new ContextInitializer(context).autoConfig();
-        } catch (Exception e) {
-            System.err.println("Error al configurar Logback: " + e.getMessage());
-        }
-
-        // Ejecuta la aplicación Spring Boot
-        SpringApplication.run(AuthmsApplication.class, args);
-    }
 }
